b18e833431b8f873e58fcf298394701178c1357f
Brendan Beard <46061381+AtlasDisease@users.noreply.github.com>
2022-02-22 17:05:24 -0600

    Version: FU 0.2.0 Gamma - Added lists

--- Examples/test.asm
+++ Examples/test.asm
@@ -5,6 +5,8 @@ SECTION .text
 global  _start                          ; Examples/test
 
 _start:                                 
+        mov     eax,[I3]                
+        mov     [I4],eax                
         Exit    {0}                     
 
 SECTION .data                           
@@ -12,19 +14,24 @@ I0      dd      5                       ; A
 B0      dd      -1                      ; B
 C0      dd      'C',0                   ; CC
 S0      dd      "Explicit Const",0      ; S
+L0      dd      1,2                     ; L
 I1      dd      4                       ; C
 B1      dd      0                       ; D
 C1      dd      'C',0                   ; CCC
 S1      dd      "Implicit Const",0      ; ST
+L1      dd      1,2                     ; LL
 
 SECTION .bss                            
 I2      resd    -5                      ; a
 B2      resd    -1                      ; b
 C2      resd    'c',0                   ; cc
 S2      resd    "Explicit Var",0        ; s
+L2      resd    1,2                     ; l
 I3      resd    -6                      ; c
 B3      resd    0                       ; d
 C3      resd    'c',0                   ; ccc
 S3      resd    "Implicit Var",0        ; st
+L3      resd    1,2                     ; ll
 B4      resd    -1                      ; E
 I4      resd    1                       ; F
+L4      resd    1,2                     ; lll
--- compiler.py
+++ compiler.py
@@ -7,9 +7,10 @@ from ste import SymbolTableEntry
 
 # --- Variables --- #
 
-TYPES = {"int", "bool", "str", "char"}#, list, tuple, set, dict, stream
+#tuple = frozen list (basically)
+TYPES = {"int", "bool", "str", "char", "list",} #set, dict, stream
 KEYWORDS = {"frozen", "not", "raise"} | TYPES #"class", "def"
-SPEC_SYMBOLS = {"=", "-", "+", "<", ">", "(", ")"}
+SPEC_SYMBOLS = {"=", "-", "+", "<", ">", "(", ")", '[', ']'}
 ERRORS = {"ArithmeticError", "AssertionError",
 					"AttributeError", "BaseException",
 					"BlockingIOError", "BrokenPipeError",
@@ -40,41 +41,41 @@ ERRORS = {"ArithmeticError", "AssertionError",
 # --- Compiler Class --- #
 
 class Compiler(object):
-	
+
 	END_OF_FILE : chr = '0x04' #My choice
-	
+
 	def __init__(self, *args):
 		"""Constructor for Compiler()"""
-		
+
 		#SymbolTable Stuff
 		self.__symbolTable : dict = {}
-		
+
 		types = tuple((type[0].upper() for type in TYPES))
 		self.__counts = dict(zip(types, (-1,)*len(types)))
 
 		#Registers
 		self.__contentsOfAReg = ""
 		self.__definedStorage = False
-		
+
 		#Stacks
 		self.__operatorStk = Stack()
 		self.__operandStk = Stack()
-		
+
 		# Lexical Stuff
 		self.__ch : chr = ''
 		self.__token : str = ""
 		self.__comment : str = ""
-		
+
 		#Error
 		self.__lineNo : int = 0
 		self.__errorCount : int = 0
 		self.__check : bool = True
-		
+
 		# Other
 		if len(args) == 2:
 			self.__title : str = args[1]
 			args = (self.__title,)*3
-		
+
 		try:
 			self.__sourceFile = open(f"{args[0]}.cmn", "r")
 			self.__listingFile = open(f"{args[1]}.ccmn", "w")
@@ -82,43 +83,46 @@ class Compiler(object):
 		except Exception:
 			print("CompilerError: Unable to open/create important files")
 			exit(1)
-		
+
 	def isBoolean(self, s : str):
 		return s == "True" or s == "False"
 
 	def isInteger(self, s : str):
 		return (s[0] == '+' or s[0] == '-' and s[1:].isdigit()) or s.isdigit()
-		
+
 	def isString(self, s : str):
 		return s[0] == '\"' and s[-1] == '\"'
-		
+
 	def isChar(self, s : str):
 		return s[0] == '\'' and s[-1] == '\''
-		
+
+	def isList(self, s : str): # NEW
+		return s[0] == '[' and s[-1] == ']'
+
 	def isSpecSymbol(self, s : str):
 		return s in SPEC_SYMBOLS
-		
+
 	def isKeyword(self, s : str):
 		return (s in KEYWORDS or self.isBoolean(s))
-		
+
 	def isNonKeyId(self, s : str):
 		return not self.isKeyword(s) and all(True if ch.isalnum() or ch == '_' else False for ch in s)
-		
+
 	def isLiteral(self, s : str):
-		return self.isInteger(s) or self.isBoolean(s) or s == "not True" or s == "not False" or self.isString(s) or self.isChar(s)
-		
+		return self.isInteger(s) or self.isBoolean(s) or s == "not True" or s == "not False" or self.isString(s) or self.isChar(s) or self.isList(s)
+
 	def isTemp(self, s : str):
 		return s[0] == "T" and s != "True"
-		
+
 	# def isConst(self, s : str):
 		# return all((True if char.isupper() or char.isdigit() or char == '_' else False for char in s))
-		
+
 	def isType(self, s : str):
 		return s in TYPES
-		
+
 	def isError(self, s : str):
 		return s in ERRORS
-		
+
 	def processError(self, err="") -> None:
 		"""Outputs error messages to .ccmn"""
 
@@ -127,20 +131,20 @@ class Compiler(object):
 		self.__listingFile.write(f"\nLine {self.__lineNo}: {err}\n\n")
 		self.__listingFile.write("Automatically generated from compiler.py...\n") # self.__createListingFooter()
 		exit(self.__errorCount)
-		
+
 	def createHeaders(self):
 		"""Creates headers from .ccmn and .asm"""
-		
+
 		time = dt.datetime.now(dt.timezone.utc)
-		
+
 		self.__listingFile.write("{}\t{}\n\n".format(self.__title, time.strftime("%Y/%m/%D %H:%M %Z")))
 		self.emitPrologue()
-		
+
 	def whichType(self, name : str):
 		"""Determines the type of a variable"""
-		
+
 		datatype : str = ""
-		
+
 		if self.isLiteral(name):
 			if self.isBoolean(name) or name == "not True" or name == "not False":
 				datatype = "BOOL"#BOOLEAN"
@@ -148,6 +152,8 @@ class Compiler(object):
 				datatype = "INT"#INTEGER"
 			elif self.isChar(name):
 				datatype = "CHAR"
+			elif self.isList(name): # NEW
+				datatype = "LIST" # NEW
 			else:
 				datatype = "STR"#STRING"
 		else:
@@ -155,29 +161,29 @@ class Compiler(object):
 				datatype = self.__symbolTable[name][1]
 			else:
 				self.processError("ReferenceError: No type determined")
-				
+
 		return datatype
-		
+
 	def whichValue(self, name : str):
 		"""Determines value of a variable"""
-		
+
 		value : str
-		
+
 		if self.isLiteral(name):
 			value = name
 		else:
-			if name in self.symbolTable:
-				value = self.symbolTable[name].value
+			if name in self.__symbolTable:
+				value = self.__symbolTable[name].value
 			else:
 				processError("ReferenceError: No type determined")
-				
+
 		return value
-		
+
 	def prog(self):
 		"""Overall program 'statement'"""
-		
+
 		type : str = ""
-		
+
 		while self.__token != self.END_OF_FILE:
 			if self.__token == "frozen": #Constant
 				self.nextToken()
@@ -197,11 +203,11 @@ class Compiler(object):
 				self.raiseStmt()
 			else:
 				self.processError(f"SyntaxError: Keyword expected, not {self.__token}")
-				
+
 	def typeStmts(self):
-		
+
 		type : str = ""
-		
+
 		if self.isType(self.__token):
 			type = self.__token.upper()
 			# type = self.__token
@@ -217,21 +223,22 @@ class Compiler(object):
 			# print(f"Explicit {str1}")
 		# else:
 			# print(f"Implicit {str1}")
-		
+
 		self.__operandStk.add(self.__token)
 		self.nextToken()
 		return type
-			
+
 	def assignStmt(self, type : str, mode : str = "VAR"):
-		
+
 		x : str
 		y : str
-		
+
 		if self.__token != "=":
 			self.processError(f"SyntaxError: Expected \'=\', got {self.__token}")
-			
+
 		y = self.nextToken()
-		if (self.__token == "+" or self.__token == "-"):
+
+		if (self.__token == "+" or self.__token == "-"): #If positive or negative int
 			self.nextToken()
 			if not self.isInteger(self.__token):
 				self.processError("SyntaxError: Integer expected after sign")
@@ -243,61 +250,68 @@ class Compiler(object):
 			if not self.isBoolean(self.__token):
 				self.processError("SyntaxError: Boolean expected after \"not\"")
 			y += f" {self.__token}"
-		if not (self.isBoolean(self.__token) or self.isInteger(self.__token) or self.isString(self.__token) or self.isChar(self.__token)):
-			self.processError(f"SyntaxError: Expected boolean, integer, string, or char, got {self.__token}")
-			
+
+		if not (self.isBoolean(self.__token) or self.isInteger(self.__token) or self.isString(self.__token) or
+		self.isChar(self.__token) or self.isList(self.__token) or self.isNonKeyId(self.__token)):
+			self.processError(f"SyntaxError: Expected boolean, integer, string, char, or list, got {self.__token}")
+
 		if (type != ""): #For explicit vars
 			if (self.whichType(self.__token) != type):
 				self.processError(f"TypeError: The stated type \"{type}\" was not the type given")
-			
+
 		x = self.__operandStk.pop()
-		self.insert(x, self.whichType(y), mode,  self.whichValue(y), "YES", 1)
+		if x not in self.__symbolTable:
+			self.insert(x, self.whichType(y), mode,  self.whichValue(y), "YES", 1)
+		else:
+			self.code("=", self.__token, x)
 		self.nextToken()
-		
+
 	def raiseStmt(self):
-		
+
 		error : str = self.__token
 		info : str = ""
-		
+
 		if not self.isError(self.__token):
 			self.processError(f"SyntaxError: Expected an error type, got {self.__token}")
-			
+
 		self.nextToken()
 		if self.__token == '(':
 			info = self.nextToken()
 			if not (self.isInteger(self.__token) or self.isString(self.__token) or self.isChar(self.__token)):
 				self.processError("SyntaxError: Illegal symbol in raise statement")
-		
+
 		self.nextToken()
 		if self.__token != ')':
 			self.processError(f"SyntaxError: Expected a ')', got {self.__token}")
-		
+
 		# self.insert("E")
 		# self.processError(f"{error}: {info}")
-		
+
 	def readStmt(self):
-		
+
 		if self.__token != ">>":
 			self.processError(f"SyntaxError: Expected \">>\", got {self.__token}")
-			
+
 		self.nextToken()
 		# operand = ids()
 		self.code(">>", self.__token, self.__operandStk.pop())
 		self.nextToken()
-		
+
 	def writeStmt(self):
-		
+
 		if self.__token != "<<":
 			self.processError(f"SyntaxError: Expected \"<<\", got {self.__token}")
-			
+
 		self.nextToken()
 		# operand = ids()
 		self.code("<<", self.__token, self.__operandStk.pop())
 		self.nextToken()
-			
+
 	def code(self, op : str, rhs : str = "", lhs : str = ""):
 		"""Placeholder for important function"""
-		if op == "<<":
+		if op == "=":
+			self.emitAssignCode(rhs, lhs)
+		elif op == "<<":
 			self.emitWriteCode(rhs, lhs)
 		elif op == ">>":
 			self.emitReadCode(rhs, lhs)
@@ -306,10 +320,10 @@ class Compiler(object):
 
 	def nextToken(self) -> str:
 		"""Determines the next token"""
-		
+
 		self.__comment = ""
 		self.__token = ""
-		
+
 		while self.__token == "":
 			if self.__ch == '#':
 				while self.__ch != '\n' and self.__ch != self.END_OF_FILE:
@@ -317,7 +331,7 @@ class Compiler(object):
 					self.nextChar()
 			elif self.__ch.isspace():
 				self.nextChar()
-			
+
 			elif self.__ch == "<":
 				self.__token += self.__ch
 				self.nextChar()
@@ -330,7 +344,7 @@ class Compiler(object):
 				if self.__ch == ">":
 					self.__token += self.__ch
 					self.nextChar()
-				
+
 			elif self.__ch == '\'':
 				self.__token += self.__ch
 				self.__token += self.nextChar()
@@ -348,7 +362,15 @@ class Compiler(object):
 					self.__token += self.__ch
 				self.__token += self.__ch
 				self.nextChar()
-				
+			elif self.__ch == '[': #NEW
+				self.__token += self.__ch
+				while self.nextChar() != ']':
+					if self.__ch == self.END_OF_FILE:
+						self.processError("EOFError: Unexpected EOF")
+					self.__token += self.__ch
+				self.__token += self.__ch
+				self.nextChar()
+
 			elif self.isSpecSymbol(self.__ch):
 				self.__token = self.__ch
 				self.nextChar()
@@ -356,7 +378,7 @@ class Compiler(object):
 				while (self.__ch.isalpha() or self.__ch.isdigit() or self.__ch == '_') and self.__ch != self.END_OF_FILE:
 					self.__token += self.__ch
 					self.nextChar()
-				
+
 				if self.__ch == self.END_OF_FILE:
 					self.processError("EOFError: Unexpected EOF")
 			elif self.__ch.isdigit():
@@ -367,33 +389,33 @@ class Compiler(object):
 				self.__token = self.__ch
 			else:
 				self.processError(f"SyntaxError: Invalid symbol {self.__ch} received...")
-		
+
 		return self.__token
-		
+
 	def nextChar(self) -> chr:
 		"""Gets the next character from the file"""
-		
+
 		self.__ch = self.__sourceFile.read(1)
 		if not self.__ch:
 			self.__ch = self.END_OF_FILE
-			
+
 		if self.__ch != self.END_OF_FILE:
 			if self.__check:
 				self.__lineNo += 1;
 				self.__listingFile.write("{:>5}| ".format(self.__lineNo))
 				self.__check = False;
-				
+
 			self.__listingFile.write(self.__ch)
 			if self.__ch == '\n':
 				self.__check = True
-			
+
 		return self.__ch
-		
+
 	def genInternalName(self, value : str):
 		"""Determines the internal name for the variable"""
-		
+
 		value = value[0].upper()
-		
+
 		self.__counts[value] += 1
 		return f"{value}{self.__counts[value]}"
 		# if value == "BOOL":
@@ -410,41 +432,41 @@ class Compiler(object):
 			# return f"S{self.__counts[value]}"
 		# else:
 			# return ""
-			
+
 	def getLabel(self):
 		"""Return a label name"""
-		
+
 		self.__labelCount += 1
 		return f".L{self.__labelCount}"
-		
+
 	def getTemp(self):
-		
+
 		temp : str = ""
-		
+
 		self.__tempNo += 1
 		temp = f"t{self.__tempNo}"
 		if self.__tempNo > self.__maxTempNo:
 			insert(temp, "UNKNOWN", "VARIABLE", "1", "NO", 1)
 			self.__maxTempNo += 1
-			
+
 		return temp
-		
+
 	def freeTemp(self):
 		"""Free temporary variable from its captivity"""
-		
+
 		self.__tempNo -= 1
 		if self.__tempNo < 0:
 			self.processError("CompilerError: tempNo should be ≥ –1")
-			
+
 	def insert(self, externalName : str, inType : str, inMode : str, inValue : str, inAlloc : str, inUnits : str):
-		
+
 		name : tuple
-		
+
 		if "," in externalName:
 			name = tuple(externalName.split(','))
 		else:
 			name = (externalName,)
-			
+
 		for n in name:
 			if self.isKeyword(n):
 				self.processError("SyntaxError: Illegal use of a keyword")
@@ -459,25 +481,27 @@ class Compiler(object):
 					inValue = '0'
 				else:
 					inValue = "-1"
-			
+			elif inType == "LIST" and inValue[0] == '[' and inValue[-1] == ']':
+				inValue = inValue[1:-1]
+
 			self.__symbolTable[n] = SymbolTableEntry(self.genInternalName(inType),inType,inMode,inValue,inAlloc,inUnits)
 			# print(self.__symbolTable[n])
-				
+
 	def emit(self, label : str = "", instruction : str = "", operands : str = "", comment : str = ""):
 		"""Output assembly code"""
 		self.__objectFile.write("{:8}{:8}{:24}{}\n".format(label, instruction, operands, comment))
-		
+
 	def emitPrologue(self):
 		"""Prologue assembly code"""
-		
+
 		self.__objectFile.write("%INCLUDE \"Along32.inc\"\n%INCLUDE \"Macros_Along.inc\"\n\n")
 		self.emit("SECTION", ".text")
 		self.emit("global", "_start", "", f"; {self.__title}\n")
 		self.emit("_start:")
-	
+
 	def emitStorage(self):
 		"""Variable storage assembly code"""
-		
+
 		self.emit("SECTION", ".data")
 		for k,v in self.__symbolTable.items():
 			if v.mode == "CONST" and v.alloc == "YES":
@@ -485,7 +509,7 @@ class Compiler(object):
 					self.emit(v.internalName, "dd", f"{v.value},0", f"; {k}")
 				else:
 					self.emit(v.internalName, "dd", v.value, f"; {k}")
-				
+
 		self.__objectFile.write("\n")
 		self.emit("SECTION", ".bss")
 		for k,v in self.__symbolTable.items():
@@ -494,55 +518,64 @@ class Compiler(object):
 					self.emit(v.internalName, "resd", f"{v.value},0", f"; {k}")
 				else:
 					self.emit(v.internalName, "resd", v.value, f"; {k}")
-				
+
+	def emitAssignCode(self, rhs : str, lhs : str):
+
+		if rhs not in self.__symbolTable: #if name is not in symbol table
+			self.processError(f"ReferenceError: {rhs} is not in symbol table") #processError(reference to undefined symbol)
+
+		if self.__contentsOfAReg != rhs:
+			self.emit("", "mov", f"eax,[{self.__symbolTable[rhs].internalName}]")
+		self.emit("", "mov", f"[{self.__symbolTable[lhs].internalName}],eax")
+
 	def emitReadCode(self, rhs : str, lhs : str):
-		
+
 		name : str = rhs
-		
+
 		if lhs not in self.__symbolTable:
 			self.processError(f"ReferenceError: {lhs} is not in symbol table")
 		if name not in self.__symbolTable: #if name is not in symbol table
 			self.processError(f"ReferenceError: {name} is not in symbol table") #processError(reference to undefined symbol)
-			
+
 		if self.whichType(name) != "INT": #"INTEGER": #if data type of name is not INTEGER
 			self.processError("can't read variables of this type")# processError(can't read variables of this type)
-			
+
 		if self.__symbolTable[name].mode != "VARIABLE": #if storage mode of name is not VARIABLE
 			self.processError("attempting to read to a read-only location") #processError(attempting to read to a read-only location)
-		
+
 		self.emit("", "call", "ReadInt", "; read int; value placed in eax") #emit code to call the Irvine ReadInt function
 		self.emit("", "mov", f"[{self.__symbolTable[name].internalName}],eax", f"; store eax at {name}") #emit code to store the contents of the A register at name
 		self.__contentsOfAReg = name # set the contentsOfAReg = name
-				
+
 	def emitWriteCode(self, rhs : str, lhs : str):
-		
+
 		name : str = rhs
-		
+
 		if lhs not in self.__symbolTable:
 			self.processError(f"ReferenceError: {lhs} is not in symbol table")
 		if name not in self.__symbolTable:
 			self.processError(f"ReferenceError: {name} is not in symbol table")
-			
+
 		if self.__contentsOfAReg != name:
 			self.emit("", "mov", f"eax,[{self.__symbolTable[name].internalName}]", f"; load {name} in eax") #emit the code to load name in the A register
 			self.__contentsOfAReg = name
-			
+
 		if self.__symbolTable[name].type == "INT": #"INTEGER":
 			self.emit("", "call", "WriteInt", "; write int in eax to standard out")
 		else:
 			self.emit("", "cmp", "eax,0", "; compare to 0") #emit code to compare the A register to 0
-			
+
 			label : str = self.getLabel() #acquire a new label Ln
 			self.emit("", "je", label, "; jump if equal to print FALSE") #emit code to jump if equal to the acquired label Ln
 			self.emit("", "mov", "edx,TRUELIT", "; load address of TRUE literal in edx") #emit code to load address of TRUE literal in the D register
-			
+
 			label2 : str = self.getLabel() #acquire a new label Ln
 			self.emit("", "jmp", label2, f"; unconditionally jump to {label2}") #emit code to unconditionally jump to label L(n + 1)
 			self.emit(f"{label}:") #emit code to label the next line with the first acquired label Ln
 			self.emit("", "mov", "edx,FALSLIT", "; load address of FALSE literal in edx") #emit code to load address of FALSE literal in the D register
 			self.emit(f"{label2}:") #emit code to label the next line with the second acquired label L(n + 1)
 			self.emit("", "call", "WriteString", "; write string to standard out") #emit code to call the Irvine WriteString function
-			
+
 			if not self.__definedStorage: #if static variable definedStorage is false
 				self.__definedStorage = True #set definedStorage to true
 				self.__objectFile.write("\n") #output an endl to objectFile
@@ -552,27 +585,27 @@ class Compiler(object):
 				self.emit("SECTION", ".text") #emit code to resume .text SECTION
 
 		self.emit("", "call", "Crlf", "; write \r\n to standard out") #emit code to call the Irvine Crlf function
-		
+
 	def emitEpilogue(self):
 		"""Output epilogue assembly code"""
-		
+
 		self.emit("", "Exit", "{0}", "\n")
 		self.emitStorage()
-	
+
 	def __main__(self):
 		"""Main Function"""
-		
+
 		self.createHeaders()
 		self.nextChar()
 		self.nextToken()
 		self.prog()
 		self.emitEpilogue()
-		
+
 		pprint(self.__symbolTable)
-			
+
 	def __del__(self):
 		"""Deconstructor for Compiler()"""
-		
+
 		self.__sourceFile.close()
 		self.__listingFile.close()
 		self.__objectFile.close()
